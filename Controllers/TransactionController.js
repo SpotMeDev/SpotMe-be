57e30240129f75d6fcf20dfb5c40e91f03af7501
Gagandeep Kang <gskang@bu.edu>
2020-11-03 17:02:35 -0500

    update transaction to have message

--- Controllers/TransactionController.js
+++ Controllers/TransactionController.js
@@ -13,12 +13,16 @@ router.post('/send', passport.authenticate('jwt', {session: false}), async (req,
     // check that the user has enough in their current balance to make that transaction
     const sender = req.user; 
     if (sender.balance >= req.body.amount) {
+        // also check for a user message 
+        if (req.body.message == "") {
+            return res.status(400).send({message: "Must include message with your transaction"}); 
+        }
         // decrement the user's balance 
         try {
-            const updatedSender = await User.findOneAndUpdate({username: sender.username}, {$inc: {balance: -req.body.amount}}, {new: true});
-            const updatedRecipient = await User.findOneAndUpdate({username: req.body.recipientUsername}, {$inc: {balance: req.body.amount}}, {new: true}); 
+            const updatedSender = await User.findOneAndUpdate({_id: sender._id}, {$inc: {balance: -req.body.amount}}, {new: true});
+            const updatedRecipient = await User.findOneAndUpdate({_id: req.body.recipientID}, {$inc: {balance: req.body.amount}}, {new: true}); 
             // create the transaction
-            const transaction = await Transaction.create({sender: updatedSender._id, recipient: updatedRecipient._id, amount: req.body.amount}); 
+            const transaction = await Transaction.create({sender: updatedSender._id, recipient: updatedRecipient._id, amount: req.body.amount, message: req.body.message}); 
             return res.status(200).send({messsage: "Succesfully completed transaction", amount: req.body.amount, balance: updatedSender.balance}); 
 
         }
@@ -34,7 +38,7 @@ router.post('/send', passport.authenticate('jwt', {session: false}), async (req,
 router.post('/add-balance', passport.authenticate('jwt', {session: false}), async (req, res) => {
     const sender = req.user; 
     try {
-        const updateUserBalance = await User.findOneAndUpdate({username: sender.username}, {$inc: {balance: req.body.amount}}, {new: true});
+        const updateUserBalance = await User.findOneAndUpdate({_id: sender._id}, {$inc: {balance: req.body.amount}}, {new: true});
         return res.status(200).send({message: "Successfully updated your balance", balance: updateUserBalance.balance});
     }
     catch (err) {
--- models/transaction.js
+++ models/transaction.js
@@ -5,6 +5,7 @@ const transactionSchema = mongoose.Schema({
     sender: {type: mongoose.Schema.Types.ObjectId, ref: 'User'}, 
     recipient: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
     amount: Number,
+    message: String, 
 }, {timestamps: true})
 
 
