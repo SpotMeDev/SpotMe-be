89714057700b3775953c614a5f1928898326c8fb
Gagandeep Kang <gskang@bu.edu>
2021-01-13 16:58:22 -0500

    refactor module.exports

--- Services/AuthService.js
+++ Services/AuthService.js
@@ -3,134 +3,141 @@ const Friends = require('../models/friends');
 const Image = require('../models/image'); 
 const Utils = require('../services/utils')
 
-
-module.exports = {
-        // check if user exists with given email
-        userWithEmail: async (email) => {
-            try {
-                // check that email is not already used 
-                const userExists = await User.findOne({email: email}); 
-                if (userExists) {
-                    return true; 
-                }
-                else {
-                    return false;
-                }
-            }
-            catch(err) {
-                return false;
-            }
-        }, 
-    // check if user already exists with given username
-    userWithUsername: async (username) => {
-        try {
-            const user = await User.findOne({username: username}); 
-            if (user) {
-                return true; 
-            }
-            else {
-                return false;
-            }
-        }
-        catch(err) {
-            return false; 
-        }
-    }, 
-
-    // function will take in the sender and recipID and then return the status of their friendship: 
-    // 0 --> not friends 
-    // 1 ---> user has requested 
-    // 2 ---> pending friend request from recipient
-    // 3 -> friends already 
-    friendStatus: async (senderId, recipientId) => {
-        // check the friends array to see if the sender is already friends with 
-        try {
-            const sender = await User.findById(senderId); 
-            const recipient = await User.findById(recipientId); 
-
-            if (sender && recipient) {
-                // now check the status of their friendship in the friends collection
-                const senderFriend = await Friends.findOne({requester: sender._id, recipient: recipient._id}); 
-                if (senderFriend) {
-                    // then return the status of the sender's friendship with the recipient 
-                    // will be either 1, 2, or 3
-                    return senderFriend.status; 
-                }
-                return 0; 
-    
-            }
-            else {
-                return 0; 
-            }   
-        }
-        catch(err) {
-            return 0; 
-        }
-    }, 
-
-    // function takes the user ID and returns all of the user's friends, in an array of objects 
-    allFriends: async (id) => {
-        const user = await User.findById(id); 
-        if (user) {
-            // create a mongo aggregation: NEED TO IMPROVE: TOO MANY QUERIES 
-            const friends = user.friends
-            if (friends.length > 0) {
-                let ret = []
-                // iterate through the friends of the user  
-                await Promise.all(friends.map(async (friend) => {
-                    // for each friend id, query for that document and the user will be the requestor so we want to map the recipient and return them
-                    let friendDoc = await Friends.findById(friend); 
-                    if (friendDoc.status == 3) {
-                        // using the id of the recipient, we will find the 
-                        let recipient = await User.findById(friendDoc.recipient);
-                        ret.push({id: recipient._id, friends: recipient.friends, name: recipient.name, username: recipient.username, email: recipient.email})
-                    }
-                }))
-                return ret; 
-            }
-            else {
-                return []
-            }
-
-        }   
-        return []; 
-
-    }, 
-
-    updateProfilePic: async (user, data) => {
-        try {
-            const buffer = Buffer.from(data, 'base64');
-            const newImage = await Image.create({img: {data: buffer, contentType: "img/jpeg"}}); 
-            const newUser = await User.findOneAndUpdate({_id: user._id}, {$set: {profileImg: newImage._id}}, {new: true}); 
+// check if user exists with given email
+let userWithEmail = async (email) => {
+    try {
+        // check that email is not already used 
+        const userExists = await User.findOne({email: email}); 
+        if (userExists) {
             return true; 
         }
-        catch (err) {
-            return false; 
+        else {
+            return false;
         }
-    }, 
-    
-    retrieveProfilePic: async (user) => {
-        const imgID = user.profileImg; 
-        // use the object ID to find the correct image document 
-        const profileImg = await Image.findById(imgID); 
-        if (profileImg) {
-            const base64 = Utils.arrayBufferToBase64(profileImg.img.data.buffer)
-            return base64
+    }
+    catch(err) {
+        return false;
+    }
+} 
+    // check if user already exists with given username
+let userWithUsername = async (username) => {
+    try {
+        const user = await User.findOne({username: username}); 
+        if (user) {
+            return true; 
         }
         else {
-            return ""
+            return false;
         }
-    }, 
-    // given a user, function returns user object details excluding password 
-    returnUserDetails: async (user, includeProfilePic = false) => {
-        if (includeProfilePic) {
-            const profilePic64 = await this.retrieveProfilePic(user)
-            return {id: user._id, name: user.name, username: user.username, email: user.email, balance: user.balance, img: profilePic64}
-        }
-        else {
-            return {id: user._id, name: user.name, username: user.username, email: user.email, balance: user.balance}
+    }
+    catch(err) {
+        return false; 
+    }
+}
+
+// function will take in the sender and recipID and then return the status of their friendship: 
+// 0 --> not friends 
+// 1 ---> user has requested 
+// 2 ---> pending friend request from recipient
+// 3 -> friends already 
+let friendStatus = async (senderId, recipientId) => {
+    // check the friends array to see if the sender is already friends with 
+    try {
+        const sender = await User.findById(senderId); 
+        const recipient = await User.findById(recipientId); 
+
+        if (sender && recipient) {
+            // now check the status of their friendship in the friends collection
+            const senderFriend = await Friends.findOne({requester: sender._id, recipient: recipient._id}); 
+            if (senderFriend) {
+                // then return the status of the sender's friendship with the recipient 
+                // will be either 1, 2, or 3
+                return senderFriend.status; 
+            }
+            return 0; 
 
         }
-    },
+        else {
+            return 0; 
+        }   
+    }
+    catch(err) {
+        return 0; 
+    }
+}
+
+// function takes the user ID and returns all of the user's friends, in an array of objects 
+let allFriends = async (id) => {
+    const user = await User.findById(id); 
+    if (user) {
+        // create a mongo aggregation: NEED TO IMPROVE: TOO MANY QUERIES 
+        const friends = user.friends
+        if (friends.length > 0) {
+            let ret = []
+            // iterate through the friends of the user  
+            await Promise.all(friends.map(async (friend) => {
+                // for each friend id, query for that document and the user will be the requestor so we want to map the recipient and return them
+                let friendDoc = await Friends.findById(friend); 
+                if (friendDoc.status == 3) {
+                    // using the id of the recipient, we will find the 
+                    let recipient = await User.findById(friendDoc.recipient);
+                    ret.push({id: recipient._id, friends: recipient.friends, name: recipient.name, username: recipient.username, email: recipient.email})
+                }
+            }))
+            return ret; 
+        }
+        else {
+            return []
+        }
+
+    }   
+    return []; 
+
+}
+
+let updateProfilePic = async (user, data) => {
+    try {
+        const buffer = Buffer.from(data, 'base64');
+        const newImage = await Image.create({img: {data: buffer, contentType: "img/jpeg"}}); 
+        const newUser = await User.findOneAndUpdate({_id: user._id}, {$set: {profileImg: newImage._id}}, {new: true}); 
+        return true; 
+    }
+    catch (err) {
+        return false; 
+    }
+} 
+
+let retrieveProfilePic = async (user) => {
+    const imgID = user.profileImg; 
+    // use the object ID to find the correct image document 
+    const profileImg = await Image.findById(imgID); 
+    if (profileImg) {
+        const base64 = Utils.arrayBufferToBase64(profileImg.img.data.buffer)
+        return base64
+    }
+    else {
+        return ""
+    }
+}
+// given a user, function returns user object details excluding password 
+let returnUserDetails = async (user, includeProfilePic = false) => {
+    if (includeProfilePic) {
+        const profilePic64 = await retrieveProfilePic(user)
+        return {id: user._id, name: user.name, username: user.username, email: user.email, balance: user.balance, img: profilePic64}
+    }
+    else {
+        return {id: user._id, name: user.name, username: user.username, email: user.email, balance: user.balance}
+
+    }
+}
+
+module.exports = {
+    userWithEmail: userWithEmail, 
+    userWithUsername: userWithUsername, 
+    friendStatus: friendStatus, 
+    allFriends: allFriends, 
+    updateProfilePic: updateProfilePic, 
+    retrieveProfilePic: retrieveProfilePic, 
+    returnUserDetails: returnUserDetails
 }
 
--- Services/utils.js
+++ Services/utils.js
@@ -1,60 +1,71 @@
 const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=';
-module.exports = {
-    dateConversion: (date) => {
-        year = date.getFullYear();
-        month = date.getMonth()+1;
-        dt = date.getDate();
 
-        if (dt < 10) {
-            dt = '0' + dt;
-        }
-        if (month < 10) {
-            month = '0' + month;
-        }
+let dateConversion = (date) => {
+  year = date.getFullYear();
+  month = date.getMonth()+1;
+  dt = date.getDate();
 
-        return year+'-' + month + '-'+dt;
-    }, 
-    btoa: (input ='')  => {
-        let str = input;
-        let output = '';
-    
-        for (let block = 0, charCode, i = 0, map = chars;
-        str.charAt(i | 0) || (map = '=', i % 1);
-        output += map.charAt(63 & block >> 8 - i % 1 * 8)) {
-    
-          charCode = str.charCodeAt(i += 3/4);
-    
-          if (charCode > 0xFF) {
-            throw new Error("'btoa' failed: The string to be encoded contains characters outside of the Latin1 range.");
-          }
-          
-          block = block << 8 | charCode;
-        }
-        
-        return output;
-      }, 
-      atob: (input = '') => {
-        let str = input.replace(/=+$/, '');
-        let output = '';
-    
-        if (str.length % 4 == 1) {
-          throw new Error("'atob' failed: The string to be decoded is not correctly encoded.");
-        }
-        for (let bc = 0, bs = 0, buffer, i = 0;
-          buffer = str.charAt(i++);
-    
-          ~buffer && (bs = bc % 4 ? bs * 64 + buffer : buffer,
-            bc++ % 4) ? output += String.fromCharCode(255 & bs >> (-2 * bc & 6)) : 0
-        ) {
-          buffer = chars.indexOf(buffer);
-        }
-    
-        return output;
-      }, 
-      arrayBufferToBase64: (buffer) => {
-        var binary = '';
-        var bytes = [].slice.call(new Uint8Array(buffer));
-        bytes.forEach((b) => binary += String.fromCharCode(b));
-        return this.btoa(binary);
+  if (dt < 10) {
+      dt = '0' + dt;
+  }
+  if (month < 10) {
+      month = '0' + month;
+  }
+
+  return year+'-' + month + '-'+dt;
+}
+
+let btoa = (input ='')  => {
+  let str = input;
+  let output = '';
+
+  for (let block = 0, charCode, i = 0, map = chars;
+  str.charAt(i | 0) || (map = '=', i % 1);
+  output += map.charAt(63 & block >> 8 - i % 1 * 8)) {
+
+    charCode = str.charCodeAt(i += 3/4);
+
+    if (charCode > 0xFF) {
+      throw new Error("'btoa' failed: The string to be encoded contains characters outside of the Latin1 range.");
     }
+    
+    block = block << 8 | charCode;
+  }
+  
+  return output;
+}
+
+let atob = (input = '') => {
+  let str = input.replace(/=+$/, '');
+  let output = '';
+
+  if (str.length % 4 == 1) {
+    throw new Error("'atob' failed: The string to be decoded is not correctly encoded.");
+  }
+  for (let bc = 0, bs = 0, buffer, i = 0;
+    buffer = str.charAt(i++);
+
+    ~buffer && (bs = bc % 4 ? bs * 64 + buffer : buffer,
+      bc++ % 4) ? output += String.fromCharCode(255 & bs >> (-2 * bc & 6)) : 0
+  ) {
+    buffer = chars.indexOf(buffer);
+  }
+
+  return output;
+} 
+
+let arrayBufferToBase64 = (buffer) => {
+  var binary = '';
+  var bytes = [].slice.call(new Uint8Array(buffer));
+  bytes.forEach((b) => binary += String.fromCharCode(b));
+  return btoa(binary);
+}
+
+
+module.exports = {
+    dateConversion: dateConversion, 
+    btoa: btoa,  
+    atob: atob, 
+    arrayBufferToBase64: arrayBufferToBase64
+    
 }
