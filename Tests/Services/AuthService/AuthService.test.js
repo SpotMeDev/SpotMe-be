ff872790bdf1b13f6735bc38166b59fb845c68f5
Gagandeep Kang <gskang@bu.edu>
2021-03-23 18:27:33 -0400

    add mocha and chai for testing

---
+++ Tests/Services/AuthService/AuthService.test.js
@@ -0,0 +1,10 @@
+const expect = require("chai").expect;
+const AuthService = require('../../../Services/AuthService');
+
+
+
+describe("Authentication Service Tests", () => {
+    it("Successful User Signup", () => {
+        expect("test").equals("test")
+    })
+})
