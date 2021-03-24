586940914a15e609abdee545e331b9313e85d0ad
Gagandeep Kang <gskang@bu.edu>
2021-03-24 13:44:08 -0400

    implement auth service test

--- Tests/Services/AuthService/AuthService.test.js
+++ Tests/Services/AuthService/AuthService.test.js
@@ -1,10 +1,38 @@
-const expect = require("chai").expect;
+const chai = require('chai'); 
+const expect = chai.expect;
+const request = require("request");
 const AuthService = require('../../../Services/AuthService');
+const mongoose = require('mongoose'); 
 
 
 
 describe("Authentication Service Tests", () => {
-    it("Successful User Signup", () => {
-        expect("test").equals("test")
+    before((done) => {
+        mongoose.connect('mongodb://localhost:27017/spotme-test', {useNewUrlParser: true}); 
+        done()
+    })
+    describe("Successful Signup Tests", () => {
+        let name = 'test'
+        let username = 'testUsername';
+        let email = 'test@gmail.com';
+        let password = 'password';
+        let token; 
+        let user; 
+        before((done) => {
+            AuthService.signupUser(name, username, email, password).then((resp) => {
+                token = resp.jwt;
+                user = resp.retUser
+                done();
+            }).catch(err => {
+                console.log(err);
+                done(err);
+            })
+        })
+        it("Successful User Signup", async () => {
+            expect(user.name).equals(name);
+            expect(user.username).equals(username);
+            expect(user.email).equals(email)
+            expect(user.balance).equals(0)
+        })
     })
 })
